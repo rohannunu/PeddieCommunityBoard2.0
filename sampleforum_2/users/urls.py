81d5355b62cec305851df1615b6c7e4ae14b8d87
rohannunu <45941834+rohannunu@users.noreply.github.com>
2021-05-11 12:00:55 -0400

    Co-authored-by: trentdma <trentdma@users.noreply.github.com>

--- sampleforum_2/users/urls.py
+++ sampleforum_2/users/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/changePassword.html')),
     path('password/', views.PasswordsChangeView.as_view(template_name='registration/changePassword.html')),
     path('passwordSuccess/', views.passwordSuccess, name='passwordSuccess'),
+    path('<int:pk>/profile/', views.ShowProfilePageView.as_view(), name='showProfilePage'),
+    path('<int:pk>/edit_profile_page/', views.EditProfilePageView.as_view(), name='editProfilePage'),
 ]
--- sampleforum_2/users/views.py
+++ sampleforum_2/users/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
-#from verify_email.email_handler import send_verification_email
+from peddieforum.models import UserProfile
+
 
 
 class UserRegisterView(generic.CreateView):
@@ -20,11 +21,26 @@ class UserEditView(generic.UpdateView):
     def get_object(self):
         return self.request.user
 
+class EditProfilePageView(generic.UpdateView):
+    model = UserProfile
+    template_name = 'registration/editProfilePage.html'
+
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
     success_url = reverse_lazy('passwordSuccess')
 
 def passwordSuccess(request):
     return render(request, 'registration/passwordSuccess.html')
+
+class ShowProfilePageView(generic.DetailView):
+    model = UserProfile
+    template_name = 'registration/userProfile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        #users = UserProfile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        current_user = get_object_or_404(UserProfile, id=self.kwargs['pk'])
+        context['current_user'] = current_user
+        return context
 
 
